c29faf1b7d166d5d7e6cd714ea5fc2d14ca0cf47
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-20 16:35:49 -0700

    Formulario de registro.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'register',
@@ -7,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
+  public user: User;
+
   constructor() { 
     this.page_title = "Registrate";
+    this.user = new User(1,'','','','','ROLE_USER','');
   }
   ngOnInit() {
+    
+  }
+
+  onSubmit(form){
+    console.log(this.user);
   }
 
 }
