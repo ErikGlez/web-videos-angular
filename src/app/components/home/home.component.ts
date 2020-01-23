267954fc2523a4ce45dd882ec738d9855d94c601
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-23 15:18:43 -0700

    Video sercive y guardar nuevos videos.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,30 +1,90 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { VideoService } from '../../services/video.service';
+
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [UserService]
+  providers: [UserService, VideoService]
 })
 export class HomeComponent implements OnInit {
-   public page_title: string;
-   public identity;
-   public token;
+  public page_title: string;
+  public identity;
+  public token;
+  public videos;
+  public page;
+  public next_page;
+  public prev_page;
+  public number_pages;
+
 
   constructor(
-    private _userService: UserService
-  ) { 
+    private _userService: UserService,
+    private _videoService: VideoService,
+    private _route: ActivatedRoute,
+    private _router: Router
+
+  ) {
     this.page_title = "Inicio";
   }
 
   ngOnInit() {
     this.loadUser();
+
+    this._route.params.subscribe(params=>{
+      var page = +params['page'];
+      
+      if(!page){
+        page =1;
+        this.prev_page = 1;
+        this.next_page = 2;
+      }
+
+      this.getVideos(page);
+    });
+    
+    
   }
 
-  loadUser(){
+  loadUser() {
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
   }
+
+  getVideos(page) {
+    this._videoService.getVideos(this.token, page).subscribe(
+      response => {
+        this.videos = response.videos;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  // Metodo para obtener la miniatura de un video de youtube
+  getThumb(url, size) {
+    var video, results, thumburl;
+
+    if (url === null) {
+      return '';
+    }
+
+    results = url.match('[\\?&]v=([^&#]*)');
+    video = (results === null) ? url : results[1];
+
+    if (size != null) {
+      thumburl = 'http://img.youtube.com/vi/' + video + '/' + size + '.jpg';
+    } else {
+      thumburl = 'http://img.youtube.com/vi/' + video + '/mqdefault.jpg';
+    }
+
+    return thumburl;
+
+  }
+
 
 }
--- src/app/components/video-new/video-new.component.ts
+++ src/app/components/video-new/video-new.component.ts
@@ -2,24 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { Video } from '../../models/video';
+import { VideoService } from '../../services/video.service';
 
 
 @Component({
   selector: 'app-video-new',
   templateUrl: './video-new.component.html',
   styleUrls: ['./video-new.component.css'],
-  providers: [ UserService ]
+  providers: [ UserService, VideoService ]
 })
 export class VideoNewComponent implements OnInit {
   public page_title: string;
   public identity;
   public token;
   public video: Video;
+  public status: string;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _videoService: VideoService
   ) { 
     this.page_title = "Guardar video favorito";
     this.identity = this._userService.getIdentity();
@@ -32,7 +35,22 @@ export class VideoNewComponent implements OnInit {
   }
 
   onSubmit(form){
-    console.log(this.video);
+    this._videoService.create(this.token, this.video).subscribe(
+      response=>{
+        if(response.status == 'success'){
+          this.status = 'success';
+          this._router.navigate(['/inicio']);
+        }else{
+          this.status = 'error';
+        }
+
+      },
+      error=>{
+        this.status = 'error';
+        console.log(error);
+
+      }
+    );
     
   }
 
